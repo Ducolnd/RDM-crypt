687b42a83fd923e9a097db02fd3fcc78493d4a64
Duco Lindhout <duco.lindhout@gmail.com>
2020-10-10 09:05:52 +0200

    Changes were made 101010

--- RDM-encrypt.py
+++ RDM-encrypt.py
@@ -1,9 +1,14 @@
+from PIL import Image
 import cv2
 import numpy as np
 
 message = "Message to be encrypted"
+password = 42
+
+np.random.seed(password)
 
 img = cv2.imread("image.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 def tobits(string):
     byte_array = []
@@ -14,15 +19,19 @@ def tobits(string):
     return byte_array
 
 byte_array = np.array(tobits(message))
-byte_array = np.resize(byte_array, img.shape)
+byte_array.resize(img.shape)
+np.random.shuffle(byte_array) # Shuffle bits 
 
 # For decryption
 # print(byte_array.reshape(-1, 8)[:20])
 
-conditions = np.where(img % 2 == 0)
-conditions2 = np.where(byte_array == 1)
-img[] += 1
-print(img)
+# Even: 1
+# Uneven: 0
+img[(img % 2 == 0) & (byte_array == 0)] += 1 # Make even number uneven
+img[(img % 2 == 1) & (byte_array == 1)] += 1 # Make uneven number evenn
 
-def encrypt(image, data):
-    pass
+# Error checking
+img[img > 255] -= 2
+
+im = Image.fromarray(img)
+im.save("outfile.png")
