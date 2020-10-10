690e8bfb0e6e1e17cde0d2914b49fb79ab3a8473
Duco Lindhout <duco.lindhout@gmail.com>
2020-10-10 16:32:29 +0200

    working decryption

--- RDM-decrypt.py
+++ RDM-decrypt.py
@@ -3,34 +3,43 @@ import numpy as np
 import binascii
 
 password = 42
+n_read = 40 # How many characters should be read
 
 img = cv2.imread("outfile.png")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 flat_img = img.ravel()
 
-#np.random.seed(password)
+np.random.seed(password)
 
-#order = np.arange(img.shape[0] * img.shape[1] * img.shape[2])
-#np.random.shuffle(order)
+order = np.arange(img.shape[0] * img.shape[1] * img.shape[2])
+np.random.shuffle(order)
 
-#data = np.zeros(img.shape).ravel()
-#for index, actualindex in np.ndenumerate(order):
-    #data[actualindex] = flat_img[index]
+#print("shape", order.shape, flat_img.shape)
 
-#print(data.reshape(img.shape))
-bits = np.copy(img)
+data = np.zeros(img.shape).ravel()
+for index, actualindex in np.ndenumerate(order):
+    data[actualindex] = flat_img[index]
+
+data.reshape(img.shape)
+bits = data
 
 bits[bits % 2 == 1] = 0
 bits[(bits % 2 == 0) & (bits > 0)] = 1
 
-byte_array = bits.reshape(-1, 8).tolist()
-by = []
 
-for byte in byte_array[:100]:
-    byte = [str(i) for i in byte]
+byte_array = bits.reshape(-1, 8).tolist()
+byte_string = ""
+
+for byte in byte_array[:n_read]:
+    byte = [str(int(i)) for i in byte]
     b = ''.join(byte)
 
-    by.append(b)
-print(by)
-print(''.join([chr(int(x, 2)) for x in by]))
+    byte_string += b
+
+byte_string = int(byte_string, 2) # To binary
+message = byte_string.to_bytes((byte_string.bit_length() + 7) // 8, "big").decode("ascii", "ignore")
+
+print(message)
+#print(by)
+#print(''.join([chr(int(x, 2)) for x in by]))
--- RDM-encrypt.py
+++ RDM-encrypt.py
@@ -15,20 +15,29 @@ def tobits(string):
     for character in string:
         bits = bin(ord(character))[2:]
         bits = '00000000'[len(bits):] + bits
+
+        bits = bits.replace("0", "2")
+        bits = bits.replace("1", "3")
+
         byte_array.append([int(b) for b in bits])
     return byte_array
 
 byte_array = np.array(tobits(message))
 byte_array.resize(img.shape)
-#np.random.shuffle(byte_array) # Shuffle bits 
+
+print(byte_array)
+
+np.random.shuffle(byte_array.ravel()) # Shuffle bits 
+byte_array.resize(img.shape)
+
 
 # For decryption
 # print(byte_array.reshape(-1, 8)[:20])
 
 # Even: 1
 # Uneven: 0
-img[(img % 2 == 0) & (byte_array == 0)] += 1 # Make even number uneven
-img[(img % 2 == 1) & (byte_array == 1)] += 1 # Make uneven number evenn
+img[(img % 2 == 0) & (byte_array == 2)] += 1 # Make even number uneven
+img[(img % 2 == 1) & (byte_array == 3)] += 1 # Make uneven number evenn
 
 # Error checking
 img[img > 255] -= 2
